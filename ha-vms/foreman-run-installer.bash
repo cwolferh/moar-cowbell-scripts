3452ec4167612967c13cd07547923b88de503f88
Crag Wolfe <cwolfe@redhat.com>
2013-10-17 04:45:46 -0400

    quoting fix, local var renamed

--- ha-vms/foreman-run-installer.bash
+++ ha-vms/foreman-run-installer.bash
@@ -3,7 +3,6 @@
 #
 FOREMAN_NODE=${FOREMAN_NODE:=s14fore1}
 MCS_SCRIPTS_DIR=${MCS_SCRIPTS_DIR:=/mnt/vm-share/mcs/ha-vms}
-VMSET_CHUNK=${VMSET_CHUNK:=s14ha1}
 # The name of the snap with the assumption that this snap has the rpms
 # openstack-foreman-installer and augeas installed, but
 # foreman_server.sh has not been run yet.
@@ -14,12 +13,12 @@ provisioning_mode=false
 
 SNAPNAME=$FOREMAN_SNAPNAME vftool.bash reboot_snap_revert $FOREMAN_NODE
 
-test_https="nc -w1 -z $FOREMAN_NODE 22"
+test_ssh="nc -w1 -z $FOREMAN_NODE 22"
 echo "waiting for ssh on $FOREMAN_NODE to come up"
 sleep 10
 exit_status=1
 while [[ $exit_status -ne 0 ]] ; do
-  eval $test_https > /dev/null
+  eval $test_ssh > /dev/null
   exit_status=$?
   echo -n .
   sleep 2
@@ -34,6 +33,6 @@ ssh -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no' -t \
     root@$FOREMAN_NODE "bash -x /mnt/vm-share/vftool/vftool.bash install_foreman_here $provisioning_mode >/tmp/$FOREMAN_NODE-install-log 2>&1"
 
 if [ $DUMP_ASTAPOR_OUTPUT = "true" ]; then
-  ssh -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no' root@$FOREMAN_NODE 'cat /tmp/$FOREMAN_NODE-install-log'
+  ssh -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no' root@$FOREMAN_NODE "cat /tmp/$FOREMAN_NODE-install-log"
 fi
 
