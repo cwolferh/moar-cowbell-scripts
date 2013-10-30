e9de56ab2a02bbbfdc50586b3f96b3444fde0eee
Crag Wolfe <cwolfe@redhat.com>
2013-10-30 02:01:23 -0400

    moar script

---
+++ run-on-vm/static-controller-ips.bash
@@ -0,0 +1,17 @@
+augtool <<EOA
+set /files/etc/sysconfig/network-scripts/ifcfg-eth1/BOOTPROTO none
+set /files/etc/sysconfig/network-scripts/ifcfg-eth1/IPADDR    192.168.200.10
+set /files/etc/sysconfig/network-scripts/ifcfg-eth1/NETMASK   255.255.255.0
+set /files/etc/sysconfig/network-scripts/ifcfg-eth1/NM_CONTROLLED no
+set /files/etc/sysconfig/network-scripts/ifcfg-eth1/ONBOOT    yes
+save
+set /files/etc/sysconfig/network-scripts/ifcfg-eth2/BOOTPROTO none
+set /files/etc/sysconfig/network-scripts/ifcfg-eth2/IPADDR    192.168.201.10
+set /files/etc/sysconfig/network-scripts/ifcfg-eth2/NETMASK   255.255.255.0
+set /files/etc/sysconfig/network-scripts/ifcfg-eth2/NM_CONTROLLED no
+set /files/etc/sysconfig/network-scripts/ifcfg-eth2/ONBOOT    yes
+save
+EOA
+
+ifup eth1
+ifup eth2
