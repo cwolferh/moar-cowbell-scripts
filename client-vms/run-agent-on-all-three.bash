74c36867fd399b95c550dbe12c89d2c4a79f7885
Crag Wolfe <cwolfe@redhat.com>
2014-07-24 01:09:10 -0400

    moar tweak

--- client-vms/run-agent-on-all-three.bash
+++ client-vms/run-agent-on-all-three.bash
@@ -1,7 +1,8 @@
+export VMSET=${VMSET:="c1a1 c1a2 c1a3"}
 thedir=/mnt/vm-share/logs/`date +%s`
 mkdir -p $thedir
 
-for vm in c1a1 c1a2 c1a3; do
+for vm in $VMSET; do
  cmd="puppet agent -tvd --trace --color=false 2>&1 | tee $thedir/$vm-puppet-out.txt"
  VMSET=$vm vftool.bash run $cmd &
  test "$vm" == "c1a3" || sleep 22.5
