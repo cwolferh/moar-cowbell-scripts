90b8eb33d34845d1c44648721d35977a6b54bc5a
Crag Wolfe <cwolfe@redhat.com>
2014-07-30 06:35:08 -0400

    moar tweak

--- ceph/simple-cluster.bash
+++ ceph/simple-cluster.bash
@@ -10,7 +10,7 @@
 # Typical order of operations:
 #  * get an HA-all-in-one-controller running on nodes $monnames
 #  * run bm-simple-server-prep.bash on the bare-metal host (make sure
-#     $nodename in that script is correct, it should be the same as 
+#     $nodename in that script is correct, it should be the same as
 #     $osdnodename below)
 #  * run this script *on the $osdnodename vm*
 
--- foreman/config/non-ha/params.bash
+++ foreman/config/non-ha/params.bash
@@ -5,6 +5,9 @@ scriptdir=$(cd $(dirname "$0"); pwd)
 if $(rpm -q --queryformat "%{RPMTAG_VERSION}" foreman | grep -qP '^(2|1.[6789])') ; then
 
   /usr/share/openstack-foreman-installer/bin/quickstack_defaults.rb \
+    -g $scriptdir/hostgroups.yaml hostgroups -v
+
+  /usr/share/openstack-foreman-installer/bin/quickstack_defaults.rb \
     -d $scriptdir/quickstack.yaml.erb \
     -g $scriptdir/hostgroups.yaml parameters -v
 
