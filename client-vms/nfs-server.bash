09d840683c7865c342bcb37eab0ae51c3dd0f610
Crag Wolfe <cwolfe@redhat.com>
2015-02-10 00:45:26 -0500

    tweak nfs-server.bash to also disable nfs v4 on el7

--- client-vms/nfs-server.bash
+++ client-vms/nfs-server.bash
@@ -2,8 +2,9 @@ export VMSET=${VMSET:=c1anfs}
 export NFS_SUBNET=${NFS_SUBNET:=192.168.0.0}
 export NFS_MASKBITS=${NFS_MASKBITS:=16}
 
-vftool.bash run "sed -i 's/#RPCNFSDARGS=\"-N 4\"/RPCNFSDARGS=\"-N 4\"/' /etc/sysconfig/nfs
+vftool.bash run "grep -q 'release 6' /etc/redhat-release && sed -i 's/#RPCNFSDARGS=\"-N 4\"/RPCNFSDARGS=\"-N 4\"/' /etc/sysconfig/nfs;
+grep -q 'release 7' /etc/redhat-release && sed -i 's/RPCNFSDARGS=\"\"/RPCNFSDARGS=\"-N 4\"/' /etc/sysconfig/nfs;
 mkdir -p /mnt/glance; chmod ug+rwx /mnt/glance; chown 161.161 /mnt/glance; echo '/mnt/glance ${NFS_SUBNET}/${NFS_MASKBITS}(rw,sync,no_root_squash)' >> /etc/exports;
 mkdir -p /mnt/cinder; chmod ug+rwx /mnt/cinder; chown 165.165  /mnt/cinder; echo '/mnt/cinder ${NFS_SUBNET}/${NFS_MASKBITS}(rw,sync,no_root_squash)' >> /etc/exports;
 /sbin/service rpcbind restart; sleep 4; /sbin/service nfs stop; sleep 4; /sbin/service nfs start; sleep 4; /sbin/service nfs restart;
-grep -q 'release 6' /etc/redhat-release &&/sbin/chkconfig nfs on"
+grep -q 'release 6' /etc/redhat-release && /sbin/chkconfig nfs on"
