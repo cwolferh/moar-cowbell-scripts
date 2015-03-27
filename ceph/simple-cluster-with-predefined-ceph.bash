ba10f91c6578493df173facbf8518860797230ab
Crag Wolfe <cwolfe@redhat.com>
2015-03-27 18:48:47 -0400

    ceph scripts adapted for latest ceph release (again)

--- ceph/simple-cluster-with-predefined-ceph.bash
+++ ceph/simple-cluster-with-predefined-ceph.bash
@@ -19,19 +19,24 @@
 #  * For now, ceph.mon.keyring needs to exist too but won't be needed
 #    when http://tracker.ceph.com/issues/9510 lands.
 
-nodenames="e1a1 e1a2 e1a3 e1a4"
-#monnames="e1a1 e1a2 e1a3"
-#monnames_to_ips="e1a1:192.168.200.10 e1a2:192.168.200.20 e1a3:192.168.200.30"
-monnames_to_ips="e1a1:192.168.200.110 e1a2:192.168.200.120 e1a3:192.168.200.130"
-firstmon=e1a1
-#nodenames="e1a1 e1a4"
-#monnames="e1a1"
-osdnodename=e1a4
-computenodes=d1a1
+nodenames="c1a1 c1a2 c1a3 c1a4"
+#monnames="c1a1 c1a2 c1a3"
+monnames_to_ips="c1a1:192.168.200.10 c1a2:192.168.200.20 c1a3:192.168.200.30"
+#monnames_to_ips="e1a1:192.168.200.110 e1a2:192.168.200.120 e1a3:192.168.200.130"
+firstmon=c1a1
+#nodenames="c1a1 c1a4"
+#monnames="c1a1"
+osdnodename=c1a4
+computenodes=c1a5
 cephconf=/mnt/vm-share/tmp/ceph.conf
 workdir=/root
 
 export PATH=$PATH:/mnt/vm-share/vftool
+
+maybe_pause() {
+  # echo 'HIT A KEY TO CONTINUE'; read  
+  /bin/true
+}
 
 # ok for rhel 7.0
 # https://gist.githubusercontent.com/cwolferh/da15f47b2659173b84fe/raw/6069e0760a95dbf54715eb67f6a2ef168312448e/ceph-firefly.repo
@@ -71,33 +76,28 @@ else
   cp -r $workdir/ceph.* /etc/ceph/
 
 fi
-echo 'HIT A KEY TO CONTINUE'; read
+maybe_pause
 
 
-ceph-deploy --ceph-conf $cephconf install $nodenames
-echo 'HIT A KEY TO CONTINUE'; read
+ceph-deploy --ceph-conf $cephconf install $nodenames $computenodes
+maybe_pause
 
 ceph-deploy --overwrite-conf mon create $monnames_to_ips
-echo 'HIT A KEY TO CONTINUE'; read
+maybe_pause
 
 ssh $firstmon 'while ! eval "ceph -s"; do sleep 5; echo .; done'
 rsync -a -e ssh root@$firstmon:/etc/ceph/ceph.client.admin.keyring /etc/ceph/
-echo 'HIT A KEY TO CONTINUE'; read
+maybe_pause
 
 ceph-deploy gatherkeys $firstmon
-echo 'HIT A KEY TO CONTINUE'; read
+maybe_pause
 
 ceph-deploy gatherkeys $osdnodename
-echo 'HIT A KEY TO CONTINUE'; read
+maybe_pause
 
 mkdir /osd0
-mkdir /osd1
-# need to specify overwrite since files are not identical bit-wise (but they are functionally identical)
 ceph-deploy --overwrite-conf osd prepare $osdnodename:/osd0
 ceph-deploy osd activate $osdnodename:/osd0
-#ceph-deploy --overwrite-conf osd prepare $osdnodename:/osd1
-#ceph-deploy osd activate $osdnodename:/osd1
-#ceph-deploy mds create $osdnodename
 
 # creating backups though may not be necessary if cinder-backup
 # service not running
@@ -105,6 +105,21 @@ ceph-deploy osd activate $osdnodename:/osd0
 ceph osd pool create images 128
 ceph osd pool create volumes 128
 #ceph osd pool create backups 128
+
+# development values!  these would be cray-cray in production
+ceph osd pool set data size 1
+ceph osd pool set metadata size 1
+ceph osd pool set rbd size 1
+ceph osd pool set images size 1
+ceph osd pool set volumes size 1
+
+mkdir /osd1
+ceph-deploy --overwrite-conf osd prepare $osdnodename:/osd1
+ceph-deploy osd activate $osdnodename:/osd1
+mkdir /osd2
+ceph-deploy --overwrite-conf osd prepare $osdnodename:/osd2
+ceph-deploy osd activate $osdnodename:/osd2
+
 
 ceph auth import -i /etc/ceph/ceph.client.images.keyring
 ceph auth import -i /etc/ceph/ceph.client.volumes.keyring
@@ -115,7 +130,7 @@ ceph auth import -i /etc/ceph/ceph.client.volumes.keyring
 #     Quickstack::Compute_common/Exec[set-secret-value virsh]
 #     /usr/bin/virsh secret-set-value --secret $(cat /etc/nova/virsh.secret) --base64 $(ceph auth get-key client.volumes)
 # Note, you may need to create /etc/ceph/ on the the compute node
-ceph-deploy --overwrite-conf admin $computenodes
+#ceph-deploy --overwrite-conf admin $computenodes
 
 # now kick the tires
 #  ceph osd lspools
@@ -128,14 +143,14 @@ ceph-deploy --overwrite-conf admin $computenodes
 #  source /root/keystonerc_admin
 # glance -d -v  image-create --name "littlecirros" --is-public true --disk-format qcow2 --container-format bare --file /mnt/vm-share/cirros-0.3.2-x86_64-disk.img
 # cinder create --display-name testv1 1
-# nova keypair-add testkey >/mnt/vm-share/nova-test.pem
+# nova keypair-add testkey >/mnt/vm-share/nova-test.pem; chmod go-rw /mnt/vm-share/nova-test.pem
 # (after compute is set up)
 # nova boot --image littlecirros --flavor m1.tiny  --key_name testkey vm1
 # nova volume-attach <nova id> <cinder id> auto
 
 # compute:
 # host group: VMSET=fore4a vftool.bash run '/mnt/vm-share/mcs/foreman/api/hosts.rb set_hostgroup "Compute (Nova Network)" d1a4'
-# install: VMSET=e1a4 vftool.bash run ceph-deploy install d1a4
+# install: VMSET=c1a5 vftool.bash run ceph-deploy install d1a4
 # on the compute node:
 # verify /etc/ceph/*
 #
